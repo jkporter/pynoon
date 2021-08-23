88f2f163031582af8574ab7e14c15da2885a041d
Jonathan Porter <mail@j.porter.name>
2021-08-23 12:56:19 -0700

    Update setuptools

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import setuptools
 
 setuptools.setup(
     name = 'pynoon',
-    version = '0.0.16',
+    version = '0.0.17',
     license = 'MIT',
     description = 'Python library for Noon Home',
     author = 'Alistair Galbraith',
     author_email = 'github@alistairs.net',
-    url = 'http://github.com/alistairg/pynoon',
+    url = 'http://github.com/jkporter/pynoon',
     include_package_data=True,
 	packages=setuptools.find_packages(),
     install_requires=['requests', 'websocket-client'],
@@ -18,7 +18,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Home Automation',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
